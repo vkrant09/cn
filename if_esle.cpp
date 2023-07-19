334e6422f295ed2c0d7facd5ca7b3f9da28226da
Vikrant Singh <vp2620777@gmail.com>
2023-07-20 01:22:28 +0530

    first

---
+++ if_esle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a , b;
+    cin>>a>>b;
+    if ( a==b){
+        cout<<"hey these are equal"<<endl;
+    }
+    else 
+    cout<<"not equal"<<endl;    
+    return 0;
+}
