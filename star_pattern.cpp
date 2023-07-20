7668f742c105d0f1bb309563607bda753ca93922
Vikrant Singh <vp2620777@gmail.com>
2023-07-20 13:49:00 +0530

    new

--- star_pattern.cpp
+++ star_pattern.cpp
@@ -44,23 +44,31 @@ using namespace std;
 ***
 */
 
-int main(){
-    int n;
-    cin>>n;
-    int i=1;
-    while(i<=n){
-        int j=1;
-        int k=1;
-        while(k<=n-i){
-            cout<<" ";
-            k++;
-        }
-        while(j<=i){
-            cout<<"*";
-            j++;
-        }
-        cout<<endl;
-        i++;
-    }
-    return 0;
-}
+// int main(){
+//     int n;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){
+//         int j=1;
+//         int k=1;
+//         while(k<=n-i){
+//             cout<<" ";
+//             k++;
+//         }
+//         while(j<=i){
+//             cout<<"*";
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+//     return 0;
+// }
+/*OUTPUT
+5
+    *
+   **
+  ***
+ ****
+*****
+*/
