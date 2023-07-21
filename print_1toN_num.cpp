356d42fcea9e300784253fea117a8ec3565249da
Vikrant Singh <vp2620777@gmail.com>
2023-07-21 23:54:37 +0530

    new

---
+++ print_1toN_num.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+void print_1_to_n(int n){
+    for (int i=0;i<=n;i++){
+        cout<<i<<end;
+    }
+}
+int main(){
+    // int a;
+    // cin>>a;
+    cout<<print_1_to_n(10);
+    return 0;
+}
