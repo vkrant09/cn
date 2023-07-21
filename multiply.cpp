6f3c568c68813b35525255bc0faf2dffec894bfa
Vikrant Singh <vp2620777@gmail.com>
2023-07-22 00:03:00 +0530

    new

---
+++ multiply.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int multiply(int a,int b){
+    int c=a*b;
+    return c;
+}
+int main(){
+    int s,d;
+    cin>>s>>d;
+    cout<<multiply(s,d);
+    return 0;
+}
